19794f5b247800e9d5268cedd87fef3cfd20a07c
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-06-11 21:12:05 -0400

    second kata done

--- conditionalSums.js
+++ conditionalSums.js
@@ -1,23 +1,27 @@
 const conditionalSum = (values, condition) => {
   // Your code here
-  let newValues = [];
+  let sum = 0;
   if (condition === 'even') {
     for (num of values) {
       if (num % 2 === 0) {
-        newValues.push(num);
+        sum += num;
       }
     }
   } else if (condition === 'odd') {
     for (num of values) {
       if (num % 2 !== 0) {
-        newValues.push(num);
+        sum += num;
       }
     }
   }
-  return newValues;
+  else {
+    return null;
+  }
+  return sum;
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], 'even'));
 console.log(conditionalSum([1, 2, 3, 4, 5], 'odd'));
 console.log(conditionalSum([13, 88, 12, 44, 99], 'even'));
 console.log(conditionalSum([], 'odd'));
+console.log(conditionalSum([], 'od'));
